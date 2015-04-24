247e4107de1a7feba1a35621ebecfb33efa555ec
Rufus Zhu <Rufus.Zhu@digital-dispatch.com>
2015-04-24 16:41:48 -0700

    more ui

--- app/src/main/java/com/example/rzhu/sepratebill/MainActivity.java
+++ app/src/main/java/com/example/rzhu/sepratebill/MainActivity.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
+    private TextView tip_20,tip_15,tip_10,tip_0,tip_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
