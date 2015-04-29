7260416c2c2b3181284b566c6be926853716302d
Rufus Zhu <Rufus.Zhu@digital-dispatch.com>
2015-04-28 18:07:38 -0700

    ATM TextView

--- app/src/main/java/com/example/rzhu/sepratebill/AtmTextView.java
+++ app/src/main/java/com/example/rzhu/sepratebill/AtmTextView.java
@@ -1,6 +1,9 @@
 package com.example.rzhu.sepratebill;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
@@ -9,6 +12,7 @@ import java.text.DecimalFormat;
  * Created by rzhu on 4/24/2015.
  */
 public class AtmTextView extends TextView {
+    private static final String TAG = AtmTextView.class.getSimpleName();
     private String text;
     private static final DecimalFormat precision = new DecimalFormat("00.00");
 
@@ -17,24 +21,65 @@ public class AtmTextView extends TextView {
         text = "";
     }
 
+    public AtmTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        text = "";
+
+    }
+
+    public AtmTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        text = "";
+    }
+
+    @Override
+    protected void onDraw (Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
     public void enterText(String s){
-        text.replace(".","");
+        text = text.replace(".","");
         text += s;
         try {
-            double value = Double.parseDouble(text);
-            value = value/100;
-            text = String.valueOf(value);
-            setText(precision.format(text));
+            Log.e(TAG,"text: " + text);
+            double value = (double) Integer.parseInt(text);
+
+            value = value/100d;
+            Log.e(TAG,"value: " + value);
+            text = precision.format(value);
+            setText(text);
         }
         catch (NumberFormatException e){
             text = "00.0" + s;
             setText(text);
         }
-
     }
 
     public void deleteText(){
+        text = text.replace(".","");
+        text = chopLastChar(text);
+        try {
+            double value = (double) Integer.parseInt(text);
+            value = value/100d;
+            text = precision.format(value);
+            setText(text);
+        }
+        catch (NumberFormatException e){
+            text = "00.00";
+            setText(text);
+        }
+    }
 
+    public void clearText(){
+        text = "00.00";
+        setText(text);
+    }
+
+    private String chopLastChar(String str) {
+        if (str.length() > 0 && str.charAt(str.length()-1)=='x') {
+            str = str.substring(0, str.length()-1);
+        }
+        return str;
     }
 
 }
--- app/src/main/java/com/example/rzhu/sepratebill/MainActivity.java
+++ app/src/main/java/com/example/rzhu/sepratebill/MainActivity.java
@@ -2,18 +2,23 @@ package com.example.rzhu.sepratebill;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-
+    private final static String TAG = MainActivity.class.getSimpleName();
     private TextView tip_20,tip_15,tip_10,tip_0,tip_btn;
     private TextView bill_clear, bill_del, bill_0, bill_1, bill_2, bill_3, bill_4, bill_5, bill_6, bill_7, bill_8, bill_9;
     private TextView tv_tip_result,tv_total_result;
     private AtmTextView tv_bill_result;
+    private TableLayout number_pad;
 
 
     @Override
@@ -34,20 +39,24 @@ public class MainActivity extends Activity {
         bill_8 = (TextView) findViewById(R.id.bill_8);
         bill_9 = (TextView) findViewById(R.id.bill_9);
 
+        number_pad = (TableLayout) findViewById(R.id.number_pad);
+
         tv_bill_result = (AtmTextView) findViewById(R.id.tv_bill_result);
         tv_tip_result = (TextView) findViewById(R.id.tv_tip_result);
         tv_total_result = (TextView) findViewById(R.id.tv_total_result);
+
+
 
         View.OnClickListener billPadListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int id = v.getId();
-                int value = Integer.parseInt(((TextView) v).getText().toString());
                 switch (id){
                     case(R.id.bill_clear):
-
+                        tv_bill_result.clearText();
                         break;
                     case(R.id.bill_del):
+                        tv_bill_result.deleteText();
                         break;
                     default:
                         tv_bill_result.enterText(((TextView) v).getText().toString());
@@ -55,6 +64,14 @@ public class MainActivity extends Activity {
                 }
             }
         };
+
+        for(int i = 0; i < number_pad.getChildCount(); i++){
+            TableRow row = (TableRow)number_pad.getChildAt(i);
+            for(int j = 0; j < row.getChildCount(); j++){
+                row.getChildAt(j).setOnClickListener(billPadListener);
+            }
+        }
+
     }
 
 
